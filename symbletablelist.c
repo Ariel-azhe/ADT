c4137942288fc3561c358468a08bd868afb8015c
Ariel Yuan <ay3052@princeton.edu>
2025-10-22 19:11:31 -0400

    added files

---
+++ symbletablelist.c
@@ -0,0 +1,126 @@
+#include <symtable.h>
+
+size_t size;
+
+SymTable_T SymTable_new(void)
+{
+    SymTable_T sym = calloc(1, sizeof(struct List));
+    if (sym == NULL)
+    {
+        return NULL;
+    }
+    return sym;
+}
+
+  void SymTable_free(SymTable_T oSymTable)
+  {
+    while (size > 0)
+    {
+        struct Node *fst_next = oSymTable->first->next;
+        free(oSymTable->first);
+        oSymTable->first = NULL;
+        oSymTable->first = fst_next;
+        size--;
+    }
+  }
+
+  size_t SymTable_getLength(SymTable_T oSymTable)
+  {
+    return size;
+  }
+
+  int SymTable_put(SymTable_T oSymTable,
+     const char *pcKey, const void *pvValue)
+     {
+        if (SymTable_contains(oSymTable, pcKey))
+        {
+            return 0;
+        }
+        else
+        {
+            struct Node *prev_fst = oSymTable->first;
+            struct Node *fst = {pcKey, pvValue, prev_fst}; 
+            oSymTable->first = fst;
+            return 1;
+        }
+     }
+
+  void *SymTable_replace(SymTable_T oSymTable,
+     const char *pcKey, const void *pvValue)
+     {
+        struct Node *cur = oSymTable->first;
+        int replaced = 0;
+        while (cur->next != NULL)
+        {
+            if (*(cur->key) == *pcKey)
+            {
+                cur->value = pvValue;
+                replaced = 1;
+            }
+            cur++;
+        }
+        if (replaced == 0)
+        {
+            return NULL;
+        }
+     }
+
+  int SymTable_contains(SymTable_T oSymTable, const char *pcKey)
+  {
+    struct Node *cur = oSymTable->first;
+    while (cur->next != NULL)
+    {
+        if (*(cur->key) == *pcKey)
+        {
+            return 1;
+        }
+        cur++;
+    }
+    return 0;
+  }
+
+  void *SymTable_get(SymTable_T oSymTable, const char *pcKey)
+  {
+    struct Node *cur = oSymTable->first;
+    while (cur->next != NULL)
+    {
+        if (*(cur->key) == *pcKey)
+        {
+            return cur->value;
+        }
+        cur++;
+    }
+    return NULL;
+  }
+
+  void *SymTable_remove(SymTable_T oSymTable, const char *pcKey)
+  {
+    struct Node *prev = oSymTable->first;
+    int removed = 0;
+    while (prev->next != NULL)
+    {
+        if (*(prev->next->key) == *pcKey)
+        {
+            struct Node *next = prev->next->next;
+            free(prev->next);
+            prev->next = next;
+            removed = 1;
+        }
+        prev++;
+    }
+    if (removed == 0)
+    {
+        return NULL;
+    }
+  }
+
+  void SymTable_map(SymTable_T oSymTable,
+     void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
+     const void *pvExtra)
+     {
+        struct Node *cur = oSymTable->first;
+        while (cur->next != NULL)
+        {
+            (*pfApply)(cur->key, cur->value, pvExtra);
+        }
+    }
---
+++ symtable.h
@@ -0,0 +1,58 @@
+
+#ifndef SYMTABLE_H
+    #define SYMTABLE_H
+    #include <stdio.h>
+    #include <stdlib.h>
+    struct Node {
+        const char *key;
+        int value;
+        struct Node *next;
+    };
+
+    struct List {
+        struct Node *first;
+    };
+    typedef struct List *SymTable_T;
+
+    /*creates new empty symbol table or returns NULL
+    if insufficient memory*/
+    SymTable_T SymTable_new(void);
+
+    /*frees all memory occupied by oSymTable*/
+  void SymTable_free(SymTable_T oSymTable);
+
+  /*returns the number of bindings in oSymTable*/
+  size_t SymTable_getLength(SymTable_T oSymTable);
+
+  /*Adds a new binding with key pcKey and value pvValue to oSymTable
+  if oSymTable doesn't contain a binding with key pcKey already.
+  Return 0 and leave oSymTable unchanged otherwise.*/
+  int SymTable_put(SymTable_T oSymTable,
+     const char *pcKey, const void *pvValue);
+    
+    /*Change the value of the binding within oSymTable
+    with key pcKey to pvValue if such binding exists in 
+    oSymTable. Return NULL otherwise.*/
+  void *SymTable_replace(SymTable_T oSymTable,
+     const char *pcKey, const void *pvValue);
+
+     /*Return 1 if oSymTable contains a binding with key pcKey.
+     Return 0 otherwise.*/
+  int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
+
+  /*Return the value of the binding with key pcKey if such key 
+  exists in oSymTable. Return NULL otherwise.*/
+  void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
+
+  /*Remove the binding with key pcKey in oSymTable and return
+  the value of that binding if that binding exists.
+  Return NULL otherwise.*/
+  void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
+
+  /*Applies function *pfApply to all bindings in oSymTable with
+  extra parameter pvExtra*/
+  void SymTable_map(SymTable_T oSymTable,
+     void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
+     const void *pvExtra);
+    
+#endif
