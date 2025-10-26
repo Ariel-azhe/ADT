dce3174cc287826495e6eda0e85b0801f3ccbaab
Ariel Yuan <ay3052@princeton.edu>
2025-10-26 15:32:58 -0400

    added main

--- symtablelist.c
+++ symtablelist.c
@@ -10,8 +10,9 @@ int main(void)
     int num = 1;
     const char *pcKey = &a;
     pvValue = &num;
-    int *pres = (int*)(SymTable_put(oSymtable, pcKey, pvValue));
-    return *pres;
+    SymTable_put(oSymtable, pcKey, pvValue);
+    int *res = (int*)(SymTable_get(oSymtable, pcKey));
+    return *res;
 }
 SymTable_T SymTable_new(void)
 {
