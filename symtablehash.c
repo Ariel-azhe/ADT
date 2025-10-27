1622979a40c92cd74b9a9d5beb3ce618ca33c238
Ariel Yuan <ay3052@princeton.edu>
2025-10-27 14:39:19 -0400

    added hash

--- symtablehash.c
+++ symtablehash.c
@@ -1,16 +1,5 @@
 #include "symtable.h"
 
-
-struct Binding {
- const char *key;
- int value;
- struct Binding *next;
-};
-struct Table {
- struct Binding *buckets[BUCKET_COUNT];
-};
-typedef struct Table *SymTable_T;
-
 size_t bindings = 0;
 int hkeys[BUCKET_COUNT];
 /*creates new empty symbol table or returns NULL
