0dbf4ccea8a2505949efe200cfb11b723f936241
Ariel Yuan <ay3052@princeton.edu>
2025-10-28 13:06:08 -0400

    added size array

--- symtablehash.c
+++ symtablehash.c
@@ -45,7 +45,7 @@ a certain amount*/
 void SymTable_expand(SymTable_T oSymTable)
 {
     struct Binding ** new_buckets = 
-    (struct Binding**)realloc(oSymTable->buckets, bucket_cnts[bindex]*2*sizeof(struct Binding));
+    (struct Binding**)realloc(oSymTable->buckets, bucket_cnts[bindex++]*sizeof(struct Binding));
     size_t i = 0;
     if (new_buckets == NULL)
     {
