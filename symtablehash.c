bd8011b79eec0a727ca04d6d58f50017b4fc10fd
Ariel Yuan <ay3052@princeton.edu>
2025-10-28 18:25:44 -0400

    test hash

--- symtablehash.c
+++ symtablehash.c
@@ -194,10 +194,11 @@ static size_t SymTable_hash(const char *pcKey, size_t uBucketCount)
 a certain amount*/
 void SymTable_expand(SymTable_T oSymTable)
 {
-    print("expanded");
     struct Binding ** new_buckets = 
     (struct Binding**)realloc(oSymTable->buckets, bucket_cnts[bindex++]*sizeof(struct Binding));
     size_t i = 0;
+    printf("expanded");
+
     if (new_buckets == NULL)
     {
         return;
