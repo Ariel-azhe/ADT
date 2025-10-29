b5b169eca09d0e3fb3b3cf2abedcff1d6833b63a
Ariel Yuan <ay3052@princeton.edu>
2025-10-29 13:10:13 -0400

    added hash

--- symtablehash.c
+++ symtablehash.c
@@ -97,13 +97,9 @@ void SymTable_expand(SymTable_T oSymTable)
                     oSymTable->buckets[i] = pnext;
                     prev = pnext;
                 }
-                else if (prev != NULL && prev->next != NULL)
-                {
-                    prev->next = pnext;
-                }
                 else
                 {
-                    break;
+                    prev->next = pnext;
                 }
                 hnext = oSymTable->buckets[hvalue];
                 nmemo = (struct Binding*)realloc((void*)cur, sizeof(struct Binding));
